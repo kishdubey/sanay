2693ff3f0afb75ad141dffade81ffbf60a179b73
Kish Dubey <dkish2000@gmail.com>
2020-11-27 19:42:37 -0500

    gitignore added, templates

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello_world():
-    return "Hello World!"
+def home():
+    return render_template("index.html")
+
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+        return render_template("chat.html", username=username, room=room)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
