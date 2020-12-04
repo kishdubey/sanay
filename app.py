a75e1bc2425f1a36aa2169d2a4a240ce30800281
Kish Dubey <dkish2000@gmail.com>
2020-12-03 19:40:45 -0500

    macro forms working, reduced code repetition

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_socketio import SocketIO
-
 from forms import *
 
 app = Flask(__name__)
 app.secret_key = 'replace later'
 
-socketio = SocketIO(app)
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    reg_form = RegistriationForm()
+    reg_form = RegistrationForm()
     if reg_form.validate_on_submit():
         return "Works"
 
@@ -19,4 +15,4 @@ def index():
 
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, EqualTo
 
-class RegistriationForm(FlaskForm):
+class RegistrationForm(FlaskForm):
     username = StringField('username_label',
         validators=[InputRequired(message="Username Required"),
         Length(min=4, max=25, message="Between 4 and 25 characters")])
