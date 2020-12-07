cc84d84f2a6b475e47cc67afb9728c4a1ea8229e
Kish Dubey <dkish2000@gmail.com>
2020-12-07 11:59:58 -0500

    removed

--- models.py
+++ models.py
@@ -11,5 +11,3 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(25), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
-
-    #db.create_all()
