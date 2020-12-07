0a97fb0a0b264e30a9b701921a2daf5b4f284846
Kish Dubey <dkish2000@gmail.com>
2020-12-07 13:54:19 -0500

    sockets started

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, current_user, login_required, logout_user
-
+from flask_scoketio import SocketIO, send
 from forms import *
 from models import *
 
@@ -9,6 +9,8 @@ app.secret_key = 'replace later'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://kfhbafarwjdqwa:f0d2463f7c659badd77f5ce43bbbf97c1035ea8c3fe11c54e9b012f45b8f79b6@ec2-54-225-214-37.compute-1.amazonaws.com:5432/da5tto5evlsm28'
 db = SQLAlchemy(app)
+
+socketio = SocketIO(app)
 
 login = LoginManager(app)
 login.init_app(app)
@@ -60,5 +62,10 @@ def logout():
     flash('Logged Out succesfully', 'success')
     return redirect(url_for('login'))
 
+@socketio.on('message')
+def message(data):
+    print(f"\n\n{data}\n\n")
+    send(data)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socket.io.run(app, debug=True)
