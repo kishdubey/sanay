4210bcb8c7cbc643990bb3e72ac554485ee5ccb0
Kish Dubey <dkish2000@gmail.com>
2020-12-18 14:59:35 -0500

    socket.io functioning properly, went down version

--- sanay/static/scripts/socketio.js
+++ sanay/static/scripts/socketio.js
@@ -1,24 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-  var socket = io;
-  socket.on('connect', function() {
+  // Connect to websocket
+  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+
+  socket.on('connect', () => {
     socket.send('Connected!');
   });
 
   socket.on('message', data => {
-    // const p = document.createElement('p');
-    // const br = document.createElement('br');
-    // p.innerHTML = data;
-    //document.querySelector('#display-message-section').append(p);
     console.log(`Message recieved: ${data}`);
-
   });
-
-  socket.on('some-event', data => {
-    console.log(data)
-  });
-
-  document.querySelector('#send-message').onclick = () => {
-    socket.send(document.querySelector('#user-message').value);
-  }
 
 })
